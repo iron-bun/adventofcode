2e7bab9e9edce24dde439172e265a7712642de30
Chris Sinclair <chris@quarterstaff.co.uk>
2022-12-16 15:58:54 +0000

    day 16 part 1. At last

--- 2022/day16.py
+++ 2022/day16.py
@@ -1,26 +1,53 @@
 #!/usr/bin/env python3
 
+def parse(data):
+    result = {}
+    for line in data:
+        if line == "\n":
+            break
+        node = {}
+        line = line.split(" ")
+        node_name = line[1]
+        node['valve'] = int(line[4].split("=")[1][:-1])
+        node['neighbours'] = list(map(lambda x: x[:-1], line[9:]))
+        result[node_name] = node
+    return result
+
 def solution1(data):
     nodes = parse(data)
     results = {}
-    
-    queue = [("AA", 30, 0)]
+    max_score = 0
+
+    queue = [("AA#", 30, 0)]
     
     while len(queue) > 0:
-        queue = sorted(queue, key = lambda x: x[1])
+        queue = sorted(queue, key = lambda x: x[2], reverse=True)
         
         node, time_left, score = queue.pop(0)
+
+        if score > max_score:
+            max_score = score
+        if time_left == 0:
+            continue
         node_name, nodes_active = node.split("#")
-        nodes_active = nodes_active.split("_")
+        nodes_active = sorted(nodes_active.split("_"))
         
-        if node not in nodes_active and nodes[node]['valve'] > 0:
-            queue.append((f"{node}#{"_".join(sorted(nodes_active + [node]))", time_left-1, time_left*nodes[node]['value']))
-        
-        for n in nodes[node]['neighbours']:
-            if n in results and results[n]['cost'] < 
-            
+        results[node] = (time_left, score)
+
+        prospective_score = score+(time_left-1)*nodes[node_name]['valve']
+        if node_name in nodes_active:
+            pass
+        elif (results[node][1] < prospective_score) or (results[node][1] == prospective_score and results[node][0] < time_left):
+            queue.append((f"{node_name}#{'_'.join(sorted(nodes_active + [node_name]))}", time_left-1, prospective_score))
+
+        for n in nodes[node_name]['neighbours']:
+            n = f"{n}#{'_'.join(nodes_active)}"
+            if n not in results or results[n][1] < score or (results[n][1] == score and results[n][0] < time_left):
+                queue.append((n, time_left-1, score))
+
     
-    return 0
+    return max_score
+
 if __name__ == "__main__":
-    with open("day_16_test.txt") as f:
+    with open("day16.txt") as f:
         print(solution1(f))
