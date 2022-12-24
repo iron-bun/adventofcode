96f55dd5f2f4b7357dd447f415150c1a96a43aa0
iron-bun <chris@quarterstaff.co.uk>
2022-12-24 23:51:40 +0000

    Updated day18. Still not right

--- 2022/day18.py
+++ 2022/day18.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 
 def parse(f):
-    space = {}
+    space = []
     for line in f:
         line = line.strip()
         coordinates = tuple(map(int, line.split(",")))
-        space[coordinates] = 1
+        space.append(coordinates)
     return space
 
 def neighbours(x, y, z):
@@ -18,7 +18,7 @@ def neighbours(x, y, z):
    
 def solution1(data):
     ans = 0
-    for x, y, z in data.keys():
+    for x, y, z in data:
         ans += 6
         for a, b, c in neighbours(x, y, z):
             if (a, b, c) in data:
@@ -32,11 +32,57 @@ def generate_check_surfaces(cube):
         #north
         yield (x, y+1, z+1, 2), (x, y+1, z, 0), (x, y, z, 4)
         #west
-        yield x-1, y, z+1, 1
-        yield x-1, y, z, 0
-        yield x, y, z, 3
+        yield (x-1, y, z+1, 1), (x-1, y, z, 0), (x, y, z, 3)
         #east
+        yield (x+1, y, z+1, 3), (x+1, y, z, 0), (x, y, z, 1) 
         #south
+        yield (x, y-1, z+1, 4), (x, y-1, z, 0), (x, y, z, 2)
+    if s == 1:
+        #top
+        yield (x+1, y, z+1, 5), (x, y, z+1, 1), (x, y, z, 0)
+        #bottom
+        yield (x+1, y, z-1, 0), (x, y, z-1, 1), (x, y, z, 5)
+        #north
+        yield (x+1, y+1, z, 2), (x, y+1, z, 1), (x, y, z, 4)
+        #south
+        yield (x+1, y-1, z, 4), (x, y-1, z, 1), (x, y, z, 2)
+    if s == 2:
+        #top
+        yield (x, y-1, z+1, 5), (x, y, z+1, 2), (x, y, z, 0)
+        #bottom
+        yield (x, y-1, z-1, 0), (x, y, z-1, 2), (x, y, z, 5)
+        #east
+        yield (x+1, y-1, z, 3), (x+1, y, z, 2), (x, y, z, 1)
+        #west
+        yield (x-1, y-1, z, 1), (x-1, y, z, 2), (x, y, z, 3)
+    if s == 3:
+        #top
+        yield (x-1, y, z+1, 5), (x, y, z+1, 3), (x, y, z, 0)
+        #bottom
+        yield (x-1, y, z-1, 0), (x, y, z-1, 3), (x, y, z, 5)
+        #north
+        yield (x-1, y+1, z, 2), (x, y+1, z, 3), (x, y, z, 4)
+        #south
+        yield (x-1, y-1, z, 4), (x, y-1, z, 3), (x, y, z, 2)
+    if s == 4:
+        #top
+        yield (x, y+1, z+1, 5), (x, y, z+1, 4), (x, y, z, 0)
+        #bottom
+        yield (x, y+1, z-1, 0), (x, y, z-1, 4), (x, y, z, 5)
+        #east
+        yield (x+1, y+1, z, 3), (x+1, y, z, 4), (x, y, z, 1)
+        #west
+        yield (x-1, y+1, z, 1), (x-1, y, z, 4), (x, y, z, 3)
+    if s == 5:
+        #north
+        yield (x, y+1, z-1, 2), (x, y+1, z, 5), (x, y, z, 4)
+        #south
+        yield (x, y-1, z-1, 4), (x, y-1, z, 5), (x, y, z, 2)
+        #east
+        yield (x+1, y, z-1, 3), (x+1, y, z, 5), (x, y, z, 1)
+        #west
+        yield (x-1, y, z-1, 1), (x-1, y, z, 5), (x, y, z, 3)
+    return
 
 def process(cube, data):
     ans = []
@@ -45,52 +91,39 @@ def process(cube, data):
        
         if s1[:3] in data:
             ans.append(s1)
-        elif s2[:2] in data:
+        elif s2[:3] in data:
             ans.append(s2)
         else:
             ans.append(s3)
     return ans
     
-def process_top(cube, data):
-    return []
-    ans = []
-    x, y, z, s = cube
-    if (x-1, y, z-1) in data:
-        ans.append((x-1, y, z-1, 1))
-    elif (x-1, y, z) in data:
-        ans.append((x-1, y, z, 5))
-    else:
-        ans.append((x, y, z, 3))
-    return ans
-
-processes = [process_top, process_east, process_south, process_west, process_north, process_down]
-
 def solution2(data):
+    resolved = set()
     for k in data:
-        tmp = k
-        break
-    x, y, z = tmp
-    while z > 0:
-        if (x, y, z) in data:
-            starting_cube = (x, y, z, 5)
-        z -= 1
-
-    queue = [starting_cube]
-    resolved = []
-    ans = 0
-
-    while len(queue) > 0:
-        cube = queue.pop()
-        ans += 1
-        resolved.append(cube)
-
-        tmp = processes[cube[3]](cube, data)
-        for t in tmp:
-            if t not in resolved:
-                queue.append(t)
-    return ans
+            
+        x, y, z = k
+        while z > 0:
+            if (x, y, z) in data:
+                starting_cube = (x, y, z, 5)
+            z -= 1
+    
+        queue = [starting_cube]
+    
+        while len(queue) > 0:
+            cube = queue.pop()
+            if cube in resolved:
+                continue
+            print(cube)
+            resolved.add(cube)
+    
+            for t in process(cube, data):
+                if t not in resolved:
+                    queue.append(t)
+    return len(resolved)
+    
 if __name__ == "__main__":
     with open("day18.txt") as f:
         data = parse(f)
     print(solution1(data))
     print(solution2(data))
+    
