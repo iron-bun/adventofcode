468b8020b400d25dad5f532f1877039382b02756
Chris Sinclair <chris@quarterstaff.co.uk>
2023-12-13 18:28:21 +0000

    2020 day 13

--- 2020/day13.py
+++ 2020/day13.py
@@ -21,8 +21,25 @@ def solution1(f):
 
     return bus*tmp
         
+def solution2(f):
+    f.readline()
+    timetable = f.readline().strip().split(",")
 
+    timetable = {int(o):int(t) for o,t in enumerate(timetable) if t != "x"}
+
+    step = 1
+    ans = 1
+
+    for idx in sorted(timetable.keys()):
+      while (ans+idx) % timetable[idx] != 0:
+        ans += step
+      step *= timetable[idx]
+
+    return ans
+    
 if __name__ == "__main__":
     with open("day13.txt") as f:
         print(solution1(f))
 
+    with open("day13.txt") as f:
+        print(solution2(f))
